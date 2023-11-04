91ab852a480ea6ab6747992eddbe267397db562e
4ever2 <3417013+4ever2@users.noreply.github.com>
2023-11-05 00:58:54 +0100

    Move changelog to CHANGELOG.md

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# Changelog
+
+## Unrelased
+
+## 1.1.8
+* Fixed bug where the final local maxima was selected from the refined curves only instead of all the curves
+* Fixed incorrect scaling of refined curves
+
+## 1.1.7
+* Average speed improvement (~1s over 4s for 1000x1000 image) by optimization of the cross-correlation calculation
+
+## 1.1.6
+* Added reference to publication
+
+## 1.1.5
+* Changed resolution criterion to max freq. instead of GM
+* Renamed variables to be consistent with Matlab code and manuscript
+
+## 1.1.4
+* Added comment and formating for release
+
+## 1.1.3
+* Added Input variable check
+
+## 1.1.2
+* Removed all logs output. Reordered results table output
+
+## 1.1.1
+* Fixed compatibility issues with micro-manager 1.4
--- ijplugin/src/main/java/imdecorr/ImageDecorrelationAnalysis_.java
+++ ijplugin/src/main/java/imdecorr/ImageDecorrelationAnalysis_.java
@@ -321,23 +321,6 @@ public class ImageDecorrelationAnalysis_ implements PlugIn {
 	}
 	
 	static void displayAbout() {
-		
-		/* 
-		 * Version 1.1.1: 	Fixed compatibility issues with micro-manager 1.4
-		 * Version 1.1.2: 	Removed all logs output. Reordered results table output
-		 * Version 1.1.3: 	Added Input variable check
-		 * Version 1.1.4: 	Added comment and formating for release
-		 * Version 1.1.5: 	Changed resolution criterion to max freq. instead of GM
-		 * 					Renamed variables to be consistent with Matlab code and manuscript
-		 * Version 1.1.6:	Added reference to publication
-		 * Version 1.1.7:	Average speed improvement (~1s over 4s for 1000x1000 image) by optimization
-		 * 					of the cross-correlation calculation
-		 * Version 1.1.8:	Fixed bug where the final local maxima was selected from the refined curves only
-		 * 					instead of all the curves
-		 * 					Fixed incorrect scaling of refined curves
-		 */
-		
-
 		 String aboutText = new String("<html><h1> About Image Decorrelation Analysis</h1>" + 
 		 						"Author : Adrien Descloux <br>" +
 		 						"LBEN STI EPFL Switzerland <br>"  +
