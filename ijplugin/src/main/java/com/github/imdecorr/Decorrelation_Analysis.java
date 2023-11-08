04f252892150d8fbf0b2d8bed1b2f06faf55894a
4ever2 <3417013+4ever2@users.noreply.github.com>
2023-11-09 00:21:08 +0100

    Add about plugin menu

--- ijplugin/src/main/java/com/github/imdecorr/Decorrelation_Analysis.java
+++ ijplugin/src/main/java/com/github/imdecorr/Decorrelation_Analysis.java
@@ -1,4 +1,38 @@
+/* 
+% 
+%   ImageDecorrelationAnalysis GUI and input manager
+%
+% ---------------------------------------
+%
+% A detailled description of the method can be found in : 
+% "Descloux, A., K. S. Grußmayer, and A. Radenovic. "Parameter-free image 
+% resolution estimation based on decorrelation analysis."
+% Nature methods (2019): 1-7."
+%
+%   Copyright © 2018 Adrien Descloux - adrien.descloux@epfl.ch, 
+%   École Polytechnique Fédérale de Lausanne, LBEN/LOB,
+%   BM 5.134, Station 17, 1015 Lausanne, Switzerland.
+%
+%  	This program is free software: you can redistribute it and/or modify
+%  	it under the terms of the GNU General Public License as published by
+% 	the Free Software Foundation, either version 3 of the License, or
+%  	(at your option) any later version.
+%
+%  	This program is distributed in the hope that it will be useful,
+%  	but WITHOUT ANY WARRANTY; without even the implied warranty of
+%  	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%  	GNU General Public License for more details.
+%
+% 	You should have received a copy of the GNU General Public License
+%  	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.github.imdecorr;
+
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.UIManager;
+import java.awt.BorderLayout;
 
 import ij.IJ;
 import ij.ImageJ;
@@ -10,8 +44,70 @@ public class Decorrelation_Analysis implements PlugIn {
 
 	@Override
 	public void run(String arg) {
+		if (arg.equals("about")) {
+			showAbout();
+			return;
+		}
+
 		// TODO Auto-generated method stub
 		throw new UnsupportedOperationException("Unimplemented method 'run'");
+	}
+
+	private String getVersion() {
+		return getClass().getPackage().getImplementationVersion();
+	}
+
+	private String getPluginName() {
+		return getClass().getPackage().getImplementationTitle();
+	}
+
+	private void showAbout() {
+		String titleText = String.format("<h1> About %s</h1>", getPluginName());
+		String versionText = String.format("Version %s <br>", getVersion());
+		String aboutText = new String("<html>" +
+				titleText + 
+				"Author : Adrien Descloux <br>" +
+				"LBEN STI EPFL Switzerland <br>" +
+				versionText +
+				"<br />" +
+				"A detailled description of the method can be found in : <br />" +
+				"Descloux, A., K. S. Grußmayer, and A. Radenovic. Parameter-free image  <br>" +
+				"resolution estimation based on decorrelation analysis." +
+				"Nature methods (2019): 1-7. <br>" +
+				"<br />" +
+				"If this software has been usefull to your research, please consider citing <br>" +
+				"<a href=\"https://www.nature.com/articles/s41592-019-0515-7#article-info\">https://doi.org/10.1038/s41592-019-0515-7</a> <br>"
+				+
+				"<br />" +
+				"<h2>License</h2>" +
+				"Copyright © 2018 Adrien Descloux - adrien.descloux@epfl.ch, <br>" +
+				"École Polytechnique Fédérale de Lausanne, LBEN/LOB,<br>" +
+				"BM 5.134, Station 17, 1015 Lausanne, Switzerland. <br>" +
+				"<br />" +
+				"This program is free software: you can redistribute it and/or modify <br>" +
+				"it under the terms of the GNU General Public License as published by<br>" +
+				"the Free Software Foundation, either version 3 of the License, or<br>" +
+				"(at your option) any later version. <br>" +
+				"<br />" +
+				"This program is distributed in the hope that it will be useful,<br>" +
+				"but WITHOUT ANY WARRANTY; without even the implied warranty of<br>" +
+				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the<br>" +
+				"GNU General Public License for more details. <br>" +
+				"<br />" +
+				"You should have received a copy of the GNU General Public License <br>" +
+				"along with this program.  If not, see <a href=\"http://www.gnu.org/licenses/\">http://www.gnu.org/licenses/</a>. </html>");
+
+		JTextPane tf = new JTextPane();
+		tf.setEditable(false);
+		tf.setBorder(null);
+		tf.setForeground(UIManager.getColor("Label.foreground"));
+		tf.setFont(UIManager.getFont("Label.font"));
+		tf.setContentType("text/html");
+		tf.setText(aboutText);
+		JFrame frame = new JFrame("About this plugin");
+		frame.add(tf, BorderLayout.CENTER);
+		frame.setSize(500, 650);
+		frame.setVisible(true);
 	}
 
 	/**
